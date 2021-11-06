fefd5bfd5c803892a4471219a89548802c446791
dayanatru28 <dayanatru28@gmail.com>
2021-11-05 19:02:42 -0500

    Insercion de carrusel de promociones front-end ciclo 3

--- ciclo3/frontendC31/src/main.js
+++ ciclo3/frontendC31/src/main.js
@@ -3,13 +3,14 @@ import App from "./App.vue";
 import appPie from "./appPie.vue"
 import router from "./router";
 
-import BootstrapVue  from 'bootstrap-vue'
-import "bootstrap/dist/css/bootstrap.min.css";
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 import Vuelidate from 'vuelidate'
 
 Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
 Vue.use(Vuelidate)
 
 Vue.config.productionTip = false;
