af5895f58df64a40a7ee2a84d6533cacae39ecfb
oniotna2021 <tonyusb@gmail.com>
2021-11-16 14:36:23 -0500

    react-strap y carrousel

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ import Products from "./Components/Products/Products";
 import ProductWrap from "./Components/Products/ProductWrap";
 import CrearProducto from "./Components/Products/CrearProducto";
 import SpecificProduct from "./Components/Products/SpecificProduct";
+import Carousel from 'react-bootstrap/Carousel'
 
 
 function App() {
   return (
+
     <div className="App">
+     
       <header>
         <nav className="gradiente navbar navbar-expand-lg navbar-light bg-light">
 
@@ -48,19 +51,47 @@ function App() {
               <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
               <button class="btn btn-outline-success" type="submit">Search</button>
          </form>
-          
-          
-          
-          
           </div>
-
-         
-         
         </div>
-
         </nav>
       </header>
 
+      <Carousel>
+  <Carousel.Item interval={3000}>
+    <img
+      className="d-block w-100"
+      src="https://solucionesamtec.com.co/galpon/banner1.jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      {/* <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={3000}>
+    <img
+      className="d-block w-100"
+      src="https://solucionesamtec.com.co/galpon/banner2.jpg"
+      alt="Second slide"
+    />
+    <Carousel.Caption>
+      {/* <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item interval={3000}>
+    <img
+      className="d-block w-100"
+      src="https://solucionesamtec.com.co/galpon/banner1.jpg"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      {/* <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+     
       <Routes>
          <Route path='/el_galpon_ciclo_4' element={<Home />}></Route>
          <Route path='/login' element={<Login />}></Route>
