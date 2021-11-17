4cc993865b4df090fa77862ae1f1c192ebac58ac
oniotna2021 <tonyusb@gmail.com>
2021-11-17 11:40:12 -0500

    Estructura de react y seccion compra con Login 3

--- src/Components/Contacto.js
+++ src/Components/Contacto.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Footer from '../templates/Footer'
+import Carrusel from '../templates/Carrusel'
 
 export default class Contacto extends Component {
     render() {
         return (
             <div>
+                <Carrusel />
                 <br />
                 <br />
                 <br />
