2bf81a0af75766c0fe1986733556269d9bc12dc5
66mayas <66cordia@gmail.com>
2021-12-04 18:45:57 -0500

    logica boton agregar carrito en favoritos

--- src/Components/Favoritos.js
+++ src/Components/Favoritos.js
@@ -5,18 +5,16 @@ import {useEffect, useState} from 'react';
 
 export default function Comprar(){
  
-  var [deseados, setDeseados]=useState([]);
+  const [deseados, setDeseados]=useState([]);
+  const [carrito, setCarrito]= useState([]);
  
   useEffect(()=>{
    llenarFavoritos()
-}); 
+   prepararCarrito()
+  });
 
-const agregarAlCarrito = ()=>{
-  alert('jajaja')
-}
-
-const llenarFavoritos=()=>{
-    const email=localStorage.getItem('email')
+  const llenarFavoritos=()=>{
+    const email=localStorage.getItem('email')//agregarla al fetch, siguiente línea, antes de la coma y con el más (+)
     fetch('http://localhost:4000/api/favoritos/',{
       METHOD: 'GET',
       headers: {
@@ -30,28 +28,55 @@ const llenarFavoritos=()=>{
     })
 }
 
-return(
-    <div>
-      <Carrusel />
-      <h3 className="mt-5">Todos tus favoritos</h3>
-    <div className="container-product d-flex mt-5">
-        { 
-      deseados.map(function(item,i){ 
-      return(
-        <div className="card dieciocho mx-2">
-          <div className="card-body">
-            <h5 className="card-title">{item.nombre}</h5>
-            <p className="card-text">precio:</p>
-            <p className="card-text">{item.precio_venta}</p>
-          </div>
-          <button className="btn btn-cart btn-product" onClick={()=> agregarAlCarrito() } >Añadir al carro</button>
-      </div>
-                )})  
+  const agregarAlCarrito = () => {
+    fetch('http://localhost:4000/api/carrito',{
+      method:'POST',
+      body: JSON.stringify(carrito),
+      headers:{
+          'Accept':'application/json',
+          'Content-Type':'application/json'
+      }
+    }).then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .then( alert(deseados.nombre + ' se agregó al carrito.'))
+    .catch(err => console.error(err))   
+  }
+
+  const prepararCarrito = () => {
+      setCarrito({ 
+              email: deseados.email,
+              id_dbproducto: deseados.id_dbproducto,
+              nombre:deseados.nombre,
+              cantidad: 1,
+              productId: deseados.productoId,
+              precio_venta: deseados.precio_venta
+            })
     }
-    </div>
-    <Footer />
-    </div>
-)
+
+  return(
+      <div>
+        <Carrusel />
+        <h3 className="mt-5">Todos tus favoritos</h3>
+      <div className="container-product d-flex mt-5">
+          { 
+        deseados.map(function(item,i){ 
+        return(
+          <div className="card dieciocho mx-2">
+            <div className="card-body">
+              <h5 className="card-title">{item.nombre}</h5>
+              <p className="card-text">precio:</p>
+              <p className="card-text">{item.precio_venta}</p>
+            </div>
+            <button className="btn btn-cart btn-product" onClick={()=> agregarAlCarrito() } >Añadir al carro</button>
+        </div>
+                  )})  
+      }
+      </div>
+      <Footer />
+      </div>
+  )
 
 }
 
