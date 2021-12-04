78a33859b605df9e607a291d47f9a4735f601e69
oniotna2021 <tonyusb@gmail.com>
2021-12-03 19:05:09 -0500

    cambio total del favoritos a hook

--- src/Components/Favoritos.js
+++ src/Components/Favoritos.js
@@ -1,32 +1,22 @@
-import React, { Component } from 'react'
 import Footer from '../templates/Footer'
-import Carrusel from '../templates/Carrusel'
-import Button from '@restart/ui/esm/Button';
+import Carrusel from '../templates/Carrusel';
+import {useEffect, useState} from 'react';
 
-export default class Favoritos extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-        email:'',
-        productoId:'',
-        id_dbproducto: '',
-        nombre:'',
-        precio_venta:0,
-        deseados:[],   
-        };
-        this.aaa = this.aaa.bind(this)
-        
+
+export default function Comprar(){
+ 
+  var [deseados, setDeseados]=useState([]);
+  var [suma, setSuma]=useState(0);
+ 
+  useEffect(()=>{
+   llenarFavoritos()
+}); 
+
+const aaa = ()=>{
+  alert('jajaja')
 }
 
-componentDidMount(){
-    this.llenarFavoritos()
-}
-
-aaa(){
-  alert('hola')
-}
-
-llenarFavoritos(){
+const llenarFavoritos=()=>{
     const email=localStorage.getItem('email')
     fetch('http://localhost:4000/api/favoritos/'+email,{
       METHOD: 'GET',
@@ -37,22 +27,17 @@ llenarFavoritos(){
     })
     .then(res => res.json())
     .then(data => {
-        this.setState({deseados: data}); 
+        setDeseados(data); 
     })
 }
 
-render() {
-    return (
-        <div>
-        <Carrusel />
-            <br />
-            <h1>Mis favoritos</h1>
-            <br />
-
-
-            <div className="containerProduct  d-flex">
-    { 
-      this.state.deseados.map(function(item,i){ 
+return(
+    <div>
+      <Carrusel />
+      <h3 className="mt-5">Todos tus favoritos</h3>
+    <div className="container-product d-flex mt-5">
+        { 
+      deseados.map(function(item,i){ 
       return(
         <div class="card dieciocho mx-4 ">
         <img src="..." class="card-img-top" alt="..."/>
@@ -60,15 +45,15 @@ render() {
           <h5 class="card-title">{item.nombre}</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
-          <button onClick={()=> this.aaa() } >zzzz</button>
+          <button onClick={()=> aaa() } >zzzz</button>
         </div>
       </div>
                 )})  
     }
     </div>
-            <Footer />
+    <Footer />
     </div>
-    )
-}
+)
+
 }
 
